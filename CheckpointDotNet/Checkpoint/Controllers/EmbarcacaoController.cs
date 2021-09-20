39724fe137033ddf6b8e1248bd042846daba2631
Eric-Pessoa <pessoa.eric@gmail.com>
2021-09-20 12:01:08 -0300

    Estrutura básica, começo cadastro

---
+++ CheckpointDotNet/Checkpoint/Controllers/EmbarcacaoController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Checkpoint.Models;
+
+namespace Checkpoint.Controllers
+{
+    public class EmbarcacaoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        [HttpGet]
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(Embarcacao embarcacao)
+        {
+            string nome = embarcacao.Nome;
+            double milhas = embarcacao.MilhasNavegadas;
+            int condicao = embarcacao.Condicao;
+            int porte = embarcacao.Porte;
+            bool turistico = embarcacao.Turistico;
+            ViewData["msg"] = $"A embarcação {embarcacao.Nome} foi cadastrada!";
+            return View();
+        }
+
+
+
+        public IActionResult Alterar()
+        {
+            return View();
+        }
+
+
+        public IActionResult Deletar()
+        {
+            return View();
+        }
+
+
+        public IActionResult Listagem()
+        {
+            return View();
+        }
+    }
+}
---
+++ CheckpointDotNet/Checkpoint/Models/Embarcacao.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Checkpoint.Models
+{
+    public class Embarcacao
+    {
+        [Display(Name = "Informe o nome da embarcação")]
+        public string Nome { get; set; }
+
+        [Display(Name = "Quantas milhas a embarcação já navegou?")]
+        public double MilhasNavegadas { get; set; }
+
+        [Display(Name = "Qual a condição da embarcação?")]
+        public int Condicao { get; set; }
+
+        [Display(Name = "Qual o porte da embarcação?")]
+        public int Porte { get; set; }
+
+        [Display(Name = "É uma embarcação para fins turísticos?")]
+        public bool Turistico { get; set; }
+
+    }
+}
